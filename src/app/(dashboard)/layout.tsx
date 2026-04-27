11b4041afe0e2006fd53c58e7d491751f2f68cbb
abdelrahman-aaa <abdelrahmanalaa670@gmail.com>
2026-04-27 23:02:14 +0300

    finish contacts-page and add sonner toast and sweet alert

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
 import Sidebar from "@/components/Dashboard/Sidebar";
-
+import { Toaster } from 'sonner';
 
 export default function DashboardLayout({
   children,
@@ -8,12 +8,21 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="relative flex h-screen overflow-hidden bg-[#0A0A0A]">
-      
+      <Toaster 
+        theme="dark" 
+        position="top-center" 
+        toastOptions={{
+          style: {
+            background: '#18181b',
+            border: '1px solid rgba(255, 255, 255, 0.08)',
+            color: '#fff',
+          },
+        }}
+      />
       <Sidebar />
       <main className="flex-1 overflow-y-auto relative z-10 custom-scrollbar">
         {children}
       </main>
-
     </div>
   );
 }
